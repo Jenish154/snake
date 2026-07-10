219df32a6b6678f4dc3d20c03597a7dca1d32d25
Jenish <62749312+Jenish154@users.noreply.github.com>
2026-07-10 20:15:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,104 +1,138 @@
-aW1wb3J0IHJhbmRvbQppbXBvcnQgcHlnYW1lCgpweWdhbWUuaW5pdCgpCgpX
-SU5fSEVJR0hUID0gNTAwCldJTl9XSURUSCA9IDUwMApCTEFDSyA9ICgwLCAw
-LCAwKQpCTFVFID0gKDAsIDAsIDI1NSkKV0hJVEUgPSAoMjU1LCAyNTUsIDI1
-NSkKR1JFRU4gPSAoMCwgMjU1LCAwKQoKY2xhc3MgU25ha2U6CiAgICBkZWYg
-X19pbml0X18oc2VsZiwgaGVhZDogdHVwbGVbaW50XSA9ICgwLDApKSAtPiBO
-b25lOgogICAgICAgIHNlbGYuaGVhZCA9IGhlYWQKICAgICAgICBzZWxmLmJv
-ZHk6IGxpc3RbdHVwbGVbaW50XV0gPSBbaGVhZF0KICAgICAgICBzZWxmLmxl
-bmd0aCA9IDEKICAgIAogICAgZGVmIGFkZF9wb3Moc2VsZiwgcG9zOiB0dXBs
-ZVtpbnRdKSAtPiBOb25lOgogICAgICAgIHNlbGYuYm9keS5hcHBlbmQocG9z
-KQogICAgICAgIHNlbGYuaGVhZCA9IHBvcwoKICAgIGRlZiByZW1fcG9zKHNl
-bGYpIC0+IHR1cGxlW2ludF06CiAgICAgICAgcmV0dXJuIHNlbGYuYm9keS5w
-b3AoMCkKCgpjbGFzcyBCb2FyZDoKICAgIGRlZiBfX2luaXRfXyhzZWxmLCBz
-dXJmYWNlOiBweWdhbWUuU3VyZmFjZSwgbnVtOiBpbnQgPSA1MCkgLT4gTm9u
-ZToKICAgICAgICBzZWxmLnJlY3RfZGljdCA9IHt9CiAgICAgICAgc2VsZi5z
-dXJmYWNlID0gc3VyZmFjZQogICAgICAgIHNlbGYucmVjdF9zaXplID0gMTAK
-ICAgICAgICBzZWxmLm51bSA9IG51bQogICAgICAgIHNlbGYuZm9udCA9IHB5
-Z2FtZS5mb250LkZvbnQoJ2ZyZWVzYW5zYm9sZC50dGYnLCAzMikKICAgICAg
-ICBzZWxmLnNuYWtlOiBTbmFrZSA9IE5vbmUKICAgICAgICBzZWxmLmZyZWVf
-c3F1YXJlczogbGlzdFt0dXBsZVtpbnRdXSA9IE5vbmUKICAgICAgICBzZWxm
-LmZvb2Q6IHR1cGxlW2ludF0gPSBOb25lCiAgICAgICAgc2VsZi5sYXN0X2Rp
-cmVjdGlvbiA9ICdyJwoKICAgIGRlZiBpbml0aWFsaXNlX2dhbWUoc2VsZikg
-LT4gTm9uZToKICAgICAgICBzZWxmLmRyYXcoKQogICAgICAgIHNlbGYuZHJh
-d19zbmFrZSgpCgogICAgZGVmIGRyYXcoc2VsZikgLT4gTm9uZToKICAgICAg
-ICBmb3IgaSBpbiByYW5nZShzZWxmLm51bSk6CiAgICAgICAgICAgIGZvciBq
-IGluIHJhbmdlKHNlbGYubnVtKToKICAgICAgICAgICAgICAgIG5ld19yZWN0
-ID0gc2VsZi5kcmF3X3JlY3QoQkxVRSwgaipzZWxmLnJlY3Rfc2l6ZSwgaSpz
-ZWxmLnJlY3Rfc2l6ZSkKICAgICAgICAgICAgICAgIHNlbGYucmVjdF9kaWN0
-W2ksIGpdID0gbmV3X3JlY3QKICAgICAgICBzZWxmLmZyZWVfc3F1YXJlcyA9
-IGxpc3Qoc2VsZi5yZWN0X2RpY3Qua2V5cygpKQogICAgICAgIHB5Z2FtZS5k
-cmF3LnJlY3Qoc2VsZi5zdXJmYWNlLCAoMjUsIDI1LCAyNSksICgyMCwgc2Vs
-Zi5udW0qc2VsZi5yZWN0X3NpemUgKyAxMCwgMTUwLCA0MCkpCiAgICAKICAg
-IGRlZiBkcmF3X3NuYWtlKHNlbGYsIHBvczogdHVwbGVbaW50XSA9ICgwLCAw
-KSkgLT4gTm9uZToKICAgICAgICBzZWxmLmRyYXdfcmVjdChXSElURSwgcG9z
-WzBdLCBwb3NbMV0pCiAgICAgICAgc2VsZi5zbmFrZSA9IFNuYWtlKHBvcykK
-ICAgICAgICBzZWxmLmZyZWVfc3F1YXJlcy5yZW1vdmUocG9zKQoKICAgIGRl
-ZiBkcmF3X3JlY3Qoc2VsZiwgY29sb3I6IHR1cGxlW2ludF0sIHg6IGludCwg
-eTogaW50KSAtPiBweWdhbWUuUmVjdDoKICAgICAgICByZXR1cm4gcHlnYW1l
-LmRyYXcucmVjdChzZWxmLnN1cmZhY2UsIGNvbG9yLCAoeCwgeSwgc2VsZi5y
-ZWN0X3NpemUsIHNlbGYucmVjdF9zaXplKSkKCiAgICBkZWYgZ2VuZXJhdGVf
-Zm9vZChzZWxmKSAtPiBOb25lOgogICAgICAgIGtleSA9IHJhbmRvbS5jaG9p
-Y2Uoc2VsZi5mcmVlX3NxdWFyZXMpCiAgICAgICAgcmFuZG9tX3NxdWFyZSA9
-IHNlbGYucmVjdF9kaWN0W2tleV0KICAgICAgICBzZWxmLmRyYXdfcmVjdChH
-UkVFTiwgcmFuZG9tX3NxdWFyZS54LCByYW5kb21fc3F1YXJlLnkpCiAgICAg
-ICAgc2VsZi5mcmVlX3NxdWFyZXMucmVtb3ZlKGtleSkKICAgICAgICBzZWxm
-LmZvb2QgPSBrZXkKICAgIAogICAgZGVmIGVhdChzZWxmKSAtPiBOb25lOgog
-ICAgICAgIGlmIHNlbGYuc25ha2UuaGVhZCA9PSBzZWxmLmZvb2Q6CiAgICAg
-ICAgICAgIHNlbGYuc25ha2UuYWRkX3BvcyhzZWxmLmZvb2QpCiAgICAgICAg
-ICAgIHgsIHkgPSBzZWxmLnJlY3RfZGljdFtzZWxmLmZvb2RdLngsIHNlbGYu
-cmVjdF9kaWN0W3NlbGYuZm9vZF0ueQogICAgICAgICAgICBzZWxmLmRyYXdf
-cmVjdChXSElURSwgeCwgeSkKICAgICAgICAgICAgc2VsZi5nZW5lcmF0ZV9m
-b29kKCkKICAgICAgICAgICAgcmV0dXJuIFRydWUKICAgIAogICAgZGVmIG1v
-dmUoc2VsZiwgcG9zOiB0dXBsZVtpbnRdKSAtPiBib29sOgogICAgICAgIGlm
-IHBvcyBpbiBzZWxmLnNuYWtlLmJvZHk6CiAgICAgICAgICAgIHJldHVybgog
-ICAgICAgIHgsIHkgPSBwb3MKICAgICAgICBpZiAoeCA8IDAgb3IgeCA+IHNl
-bGYubnVtLTEpIG9yICh5IDwgMCBvciB5ID4gc2VsZi5udW0tMSk6CiAgICAg
-ICAgICAgIHJldHVybgogICAgICAgIHNlbGYuc25ha2UuYWRkX3Bvcyhwb3Mp
-CiAgICAgICAgeCwgeSA9IHNlbGYucmVjdF9kaWN0W3Bvc10ueCwgc2VsZi5y
-ZWN0X2RpY3RbcG9zXS55CiAgICAgICAgc2VsZi5kcmF3X3JlY3QoV0hJVEUs
-IHgsIHkpCiAgICAgICAgZnJlZWRfc3F1YXJlID0gc2VsZi5zbmFrZS5yZW1f
-cG9zKCkKICAgICAgICBzZWxmLmRyYXdfcmVjdChCTFVFLCBzZWxmLnJlY3Rf
-ZGljdFtmcmVlZF9zcXVhcmVdLngsIHNlbGYucmVjdF9kaWN0W2ZyZWVkX3Nx
-dWFyZV0ueSkKICAgICAgICBzZWxmLmZyZWVfc3F1YXJlcy5hcHBlbmQoZnJl
-ZWRfc3F1YXJlKQogICAgICAgIHJldHVybiBUcnVlCgpib2FyZCA9IHB5Z2Ft
-ZS5kaXNwbGF5LnNldF9tb2RlKChXSU5fV0lEVEgsIFdJTl9IRUlHSFQgKyAx
-MDApKQpjbG9jayA9IHB5Z2FtZS50aW1lLkNsb2NrKCkKcHlnYW1lLmRpc3Bs
-YXkuc2V0X2NhcHRpb24oJ1NuYWtlIEdhbWUnKQoKZGVmIG1haW4oKSAtPiBO
-b25lOgogICAgcnVuID0gVHJ1ZQogICAgZ2FtZSA9IEJvYXJkKGJvYXJkKQog
-ICAgZ2FtZS5pbml0aWFsaXNlX2dhbWUoKQogICAgZ2FtZS5nZW5lcmF0ZV9m
-b29kKCkKICAgIHdoaWxlIHJ1bjoKICAgICAgICBjbG9jay50aWNrKDEwKQog
-ICAgICAgIGlmIGdhbWUubGFzdF9kaXJlY3Rpb24gPT0gJ3UnOgogICAgICAg
-ICAgICBpZiBub3QgZ2FtZS5tb3ZlKChnYW1lLnNuYWtlLmhlYWRbMF0tMSwg
-Z2FtZS5zbmFrZS5oZWFkWzFdKSk6CiAgICAgICAgICAgICAgICBydW4gPSBG
-YWxzZQogICAgICAgIGVsaWYgZ2FtZS5sYXN0X2RpcmVjdGlvbiA9PSAnZCc6
-CiAgICAgICAgICAgIGlmIG5vdCBnYW1lLm1vdmUoKGdhbWUuc25ha2UuaGVh
-ZFswXSsxLCBnYW1lLnNuYWtlLmhlYWRbMV0pKToKICAgICAgICAgICAgICAg
-IHJ1biA9IEZhbHNlCiAgICAgICAgZWxpZiBnYW1lLmxhc3RfZGlyZWN0aW9u
-ID09ICdyJzoKICAgICAgICAgICAgaWYgbm90IGdhbWUubW92ZSgoZ2FtZS5z
-bmFrZS5oZWFkWzBdLCBnYW1lLnNuYWtlLmhlYWRbMV0rMSkpOgogICAgICAg
-ICAgICAgICAgcnVuID0gRmFsc2UKICAgICAgICBlbGlmIGdhbWUubGFzdF9k
-aXJlY3Rpb24gPT0gJ2wnOgogICAgICAgICAgICBpZiBub3QgZ2FtZS5tb3Zl
-KChnYW1lLnNuYWtlLmhlYWRbMF0sIGdhbWUuc25ha2UuaGVhZFsxXS0xKSk6
-CiAgICAgICAgICAgICAgICBydW4gPSBGYWxzZQogICAgICAgIGZvciBldiBp
-biBweWdhbWUuZXZlbnQuZ2V0KCk6CiAgICAgICAgICAgIGlmIGV2LnR5cGUg
-PT0gcHlnYW1lLlFVSVQ6CiAgICAgICAgICAgICAgICBydW4gPSBGYWxzZQog
-ICAgICAgICAgICAKICAgICAgICAgICAgaWYgZXYudHlwZSA9PSBweWdhbWUu
-S0VZRE9XTjoKICAgICAgICAgICAgICAgIGlmIGV2LmtleSA9PSBweWdhbWUu
-S19VUDoKICAgICAgICAgICAgICAgICAgICBpZiBnYW1lLmxhc3RfZGlyZWN0
-aW9uID09ICdkJzoKICAgICAgICAgICAgICAgICAgICAgICAgY29udGludWUK
-ICAgICAgICAgICAgICAgICAgICBnYW1lLmxhc3RfZGlyZWN0aW9uID0gJ3Un
-CiAgICAgICAgICAgICAgICBpZiBldi5rZXkgPT0gcHlnYW1lLktfRE9XTjoK
-ICAgICAgICAgICAgICAgICAgICBpZiBnYW1lLmxhc3RfZGlyZWN0aW9uID09
-ICd1JzoKICAgICAgICAgICAgICAgICAgICAgICAgY29udGludWUKICAgICAg
-ICAgICAgICAgICAgICBnYW1lLmxhc3RfZGlyZWN0aW9uID0gJ2QnCiAgICAg
-ICAgICAgICAgICBpZiBldi5rZXkgPT0gcHlnYW1lLktfUklHSFQ6CiAgICAg
-ICAgICAgICAgICAgICAgaWYgZ2FtZS5sYXN0X2RpcmVjdGlvbiA9PSAnbCc6
-CiAgICAgICAgICAgICAgICAgICAgICAgIGNvbnRpbnVlCiAgICAgICAgICAg
-ICAgICAgICAgZ2FtZS5sYXN0X2RpcmVjdGlvbiA9ICdyJyAgICAgICAgICAg
-ICAgICAKICAgICAgICAgICAgICAgIGlmIGV2LmtleSA9PSBweWdhbWUuS19M
-RUZUOgogICAgICAgICAgICAgICAgICAgIGlmIGdhbWUubGFzdF9kaXJlY3Rp
-b24gPT0gJ3InOgogICAgICAgICAgICAgICAgICAgICAgICBjb250aW51ZQog
-ICAgICAgICAgICAgICAgICAgIGdhbWUubGFzdF9kaXJlY3Rpb24gPSAnbCcK
-CiAgICAgICAgZ2FtZS5lYXQoKQogICAgICAgIAogICAgICAgIHB5Z2FtZS5k
-aXNwbGF5LmZsaXAoKQoKaWYgX19uYW1lX18gPT0gIl9fbWFpbl9fIjoKICAg
-IG1haW4oKQ==
+import random
+import pygame
+
+pygame.init()
+
+WIN_HEIGHT = 500
+WIN_WIDTH = 500
+BLACK = (0, 0, 0)
+BLUE = (0, 0, 255)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+
+class Snake:
+    def __init__(self, head: tuple[int] = (0,0)) -> None:
+        self.head = head
+        self.body: list[tuple[int]] = [head]
+        self.length = 1
+    
+    def add_pos(self, pos: tuple[int]) -> None:
+        self.body.append(pos)
+        self.head = pos
+
+    def rem_pos(self) -> tuple[int]:
+        return self.body.pop(0)
+
+
+class Board:
+    def __init__(self, surface: pygame.Surface, num: int = 50) -> None:
+        self.rect_dict = {}
+        self.surface = surface
+        self.rect_size = 10
+        self.num = num
+        self.font = pygame.font.Font('freesansbold.ttf', 32)
+        self.snake: Snake = None
+        self.free_squares: list[tuple[int]] = None
+        self.food: tuple[int] = None
+        self.last_direction = 'r'
+
+    def initialise_game(self) -> None:
+        self.draw()
+        self.draw_snake()
+
+    def draw(self) -> None:
+        for i in range(self.num):
+            for j in range(self.num):
+                new_rect = self.draw_rect(BLUE, j*self.rect_size, i*self.rect_size)
+                self.rect_dict[i, j] = new_rect
+        self.free_squares = list(self.rect_dict.keys())
+        pygame.draw.rect(self.surface, (25, 25, 25), (20, self.num*self.rect_size + 10, 150, 40))
+    
+    def draw_snake(self, pos: tuple[int] = (0, 0)) -> None:
+        self.draw_rect(WHITE, pos[0], pos[1])
+        self.snake = Snake(pos)
+        self.free_squares.remove(pos)
+
+    def draw_rect(self, color: tuple[int], x: int, y: int) -> pygame.Rect:
+        return pygame.draw.rect(self.surface, color, (x, y, self.rect_size, self.rect_size))
+
+    def generate_food(self) -> None:
+        key = random.choice(self.free_squares)
+        random_square = self.rect_dict[key]
+        self.draw_rect(GREEN, random_square.x, random_square.y)
+        self.free_squares.remove(key)
+        self.food = key
+    
+    def eat(self) -> None:
+        if self.snake.head == self.food:
+            self.snake.add_pos(self.food)
+            x, y = self.rect_dict[self.food].x, self.rect_dict[self.food].y
+            self.draw_rect(WHITE, x, y)
+            self.generate_food()
+            return True
+    
+    def move(self, pos: tuple[int]) -> bool:
+        if pos in self.snake.body:
+            return
+        x, y = pos
+        if (x < 0 or x > self.num-1) or (y < 0 or y > self.num-1):
+            return
+        self.snake.add_pos(pos)
+        x, y = self.rect_dict[pos].x, self.rect_dict[pos].y
+        self.draw_rect(WHITE, x, y)
+        freed_square = self.snake.rem_pos()
+        self.draw_rect(BLUE, self.rect_dict[freed_square].x, self.rect_dict[freed_square].y)
+        self.free_squares.append(freed_square)
+        return True
+
+board = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT + 100))
+clock = pygame.time.Clock()
+pygame.display.set_caption('Snake Game')
+
+def main() -> None:
+    run = True
+    game = Board(board)
+    game.initialise_game()
+    game.generate_food()
+    while run:
+        clock.tick(10)
+        if game.last_direction == 'u':
+            if not game.move((game.snake.head[0]-1, game.snake.head[1])):
+                run = False
+        elif game.last_direction == 'd':
+            if not game.move((game.snake.head[0]+1, game.snake.head[1])):
+                run = False
+        elif game.last_direction == 'r':
+            if not game.move((game.snake.head[0], game.snake.head[1]+1)):
+                run = False
+        elif game.last_direction == 'l':
+            if not game.move((game.snake.head[0], game.snake.head[1]-1)):
+                run = False
+        for ev in pygame.event.get():
+            if ev.type == pygame.QUIT:
+                run = False
+            
+            if ev.type == pygame.KEYDOWN:
+                if ev.key == pygame.K_UP:
+                    if game.last_direction == 'd':
+                        continue
+                    game.last_direction = 'u'
+                if ev.key == pygame.K_DOWN:
+                    if game.last_direction == 'u':
+                        continue
+                    game.last_direction = 'd'
+                if ev.key == pygame.K_RIGHT:
+                    if game.last_direction == 'l':
+                        continue
+                    game.last_direction = 'r'                
+                if ev.key == pygame.K_LEFT:
+                    if game.last_direction == 'r':
+                        continue
+                    game.last_direction = 'l'
+
+        game.eat()
+        
+        pygame.display.flip()
+
+if __name__ == "__main__":
+    main()
